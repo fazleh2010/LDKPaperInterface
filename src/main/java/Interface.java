d290d98f8b4a748082e8f0ee13cc91369c5569fc
elahi <fazleh2010@gmail.com>
2021-05-18 13:00:01 +0200

    refactor

--- src/main/java/Interface.java
+++ src/main/java/Interface.java
@@ -136,7 +136,8 @@ public class Interface {
         //System.out.println("noun:" + noun.toString());
         //System.out.println("verb:" + verb.toString());
         //System.out.println(parts_of_speech);
-       System.out.println(prediction + " " + interestingness + " " + lexicalElement + " " + parts_of_speech);
+       
+        System.out.println(prediction + " " + interestingness + " " + lexicalElement + " " + parts_of_speech);
         if (str.length < 3) {
             throw new Exception("less number of argument!!!");
         } else {
